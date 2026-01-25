6b230aa1a87a6b4ef0a0f8c8263c5393e64a49d8
Hariprasath2611 <hp5173690@gmail.com>
2026-01-26 00:07:52 +0530

    Updated project files

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,20 +7,7 @@ import react from "@astrojs/react";
 export default defineConfig({
   output: "static",
   integrations: [
-    {
-      name: "framewire",
-      hooks: {
-        "astro:config:setup": ({ injectScript, command }) => {
-          if (command === "dev") {
-            injectScript(
-              "page",
-              `import loadFramewire from "framewire.js";
-              loadFramewire(true);`
-            );
-          }
-        },
-      },
-    },
+
     tailwind(),
     react(),
   ],
